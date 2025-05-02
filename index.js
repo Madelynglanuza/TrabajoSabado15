6512cae86a7ae269e6ca8dcbfcd4d6ffa145c383
Madelyng Lanuza <est.mgamadorl0201@uml.edu.ni>
2025-05-01 20:09:44 -0600

    Actualizacionde cambios en .env, index.js y respaldo en controllers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+//postgresql://madelyng:pjJHLSAgT79xPlqXY3f4SJ4YVTKM84hq@dpg-d06jq1ili9vc73ef5qjg-a.oregon-postgres.render.com/biblioteca_f9hp
 const PORT = process.env.PORT;
 
 app.use(cors());
@@ -15,9 +16,9 @@ import prestamosRouter from './routes/prestamosRoute.js';
 import usuariosRouter from './routes/usuariosRoute.js';
 
 // Usa las rutas
-app.use('/api/editorial', editorialRouter);
-app.use('/api/prestamo', prestamosRouter);
-app.use('/api/usuarios', usuariosRouter);
+app.use('/editoriales', editorialRouter);
+app.use('/prestamos', prestamosRouter);
+app.use('/usuarios', usuariosRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
